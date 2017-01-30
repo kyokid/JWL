24ccbb1ec8a8ee6e720da94c94678a52fe221c7d
thiendn160794 <hoinganmotti@gmail.com>
2017-01-30 14:14:45 +0700

    [WIP]Generate QR code

--- Mobile_App/Android/CapstoneLibrary/app/src/main/java/com/auto/jarvis/libraryicognite/BarCodeActivity.java
+++ Mobile_App/Android/CapstoneLibrary/app/src/main/java/com/auto/jarvis/libraryicognite/BarCodeActivity.java
@@ -6,6 +6,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
@@ -16,7 +17,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
-import com.auto.jarvis.libraryicognite.adapters.PagerFragmentAdapter;
 import com.auto.jarvis.libraryicognite.models.input.User;
 
 import butterknife.BindView;
@@ -35,6 +35,7 @@ public class BarCodeActivity extends AppCompatActivity {
     NavigationView navigationView;
     private ActionBarDrawerToggle actionBarDrawerToggle;
     String[] tabTitle;
+    FragmentPagerAdapter adapterViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,12 +57,12 @@ public class BarCodeActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
 
-        viewPager.setAdapter(new PagerFragmentAdapter(getSupportFragmentManager()));
+        adapterViewPager = new MyPagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(adapterViewPager);
         tabLayout.setupWithViewPager(viewPager);
         for (int i = 0; i < tabLayout.getTabCount(); i++) {
 //            tabLayout.getTabAt(i).setIcon(icons[i]);
             tabLayout.getTabAt(i).setText(tabTitle[i].toString());
-
         }
 
         View headerLayout = navigationView.inflateHeaderView(R.layout.drawer_header);
