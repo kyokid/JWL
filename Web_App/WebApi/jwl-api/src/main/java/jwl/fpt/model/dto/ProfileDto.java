f62cfc9137e9a27d01852b1f22954d6f8fb038ce
thiendn160794 <hoinganmotti@gmail.com>
2017-02-07 08:48:28 +0700

    Commit ProfileDto

---
+++ Web_App/WebApi/jwl-api/src/main/java/jwl/fpt/model/dto/ProfileDto.java
@@ -0,0 +1,21 @@
+package jwl.fpt.model.dto;
+
+import jwl.fpt.entity.AccountEntity;
+import lombok.Data;
+
+import java.sql.Date;
+
+/**
+ * Created by thiendn on 07/02/2017.
+ */
+@Data
+public class ProfileDto {
+    private String userId;
+    private String fullname;
+    private String email;
+    private String address;
+    private Date dateOfBirth;
+    private String phoneNo;
+    private String placeOfWork;
+    //private AccountEntity account;
+}
