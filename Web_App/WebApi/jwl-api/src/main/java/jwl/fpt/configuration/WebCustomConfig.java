8c4394c482b19679352ac313604abbeb56893df7
Enta'ard <dratannta@gmail.com>
2017-02-13 13:23:52 +0700

    Add CORS interceptor to allow CORS api call

---
+++ Web_App/WebApi/jwl-api/src/main/java/jwl/fpt/configuration/WebCustomConfig.java
@@ -0,0 +1,16 @@
+package jwl.fpt.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by Entaard on 2/13/17.
+ */
+@Configuration
+public class WebCustomConfig extends WebMvcConfigurerAdapter {
+        @Override
+        public void addInterceptors(InterceptorRegistry registry) {
+            registry.addInterceptor(new CorsInterceptor());
+        }
+}
