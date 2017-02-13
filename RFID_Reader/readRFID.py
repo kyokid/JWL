782f6e782fe3bfe0077c1f507216cf79a176f08e
Hong Ha Vo <havh0108@gmail.com>
2017-02-13 10:20:24 +0700

    update ibeaconId: now use minor instead of macAddress. 1 for init and 3 for finish check out

--- RFID_Reader/readRFID.py
+++ RFID_Reader/readRFID.py
@@ -3,11 +3,12 @@ import json
 import serial
 
 def senPostRequest(data):
-    print data
+    dataString = data[1:-1]
+    print str(dataString)
     url = 'https://jwl-api-v0.herokuapp.com/add/copy'
     data = {
-        "ibeaconId": "D8:CF:F3:6B:8E:01",
-        "rfid": data
+        "ibeaconId" : "1",
+        "rfid" : dataString
     }
     headers = {'Content-Type': 'application/json'}
     response = requests.post(url, data=json.dumps(data), headers=headers)
@@ -18,7 +19,7 @@ def senPostRequest(data):
 ser = serial.Serial('/dev/tty.usbserial-A702RZ3Y')  # open serial port
 print(ser.name)         # check which port was really used
 while True:
-    x = ser.read(12)
+    x = ser.read(12)        
     print "/"+x+"/"
     senPostRequest(x)
 ser.close()             # close port
